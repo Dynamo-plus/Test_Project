9c948415fb88715b55ef0648c2b8abe4fad0ba1f
Dynamo-plus <abayomisundaygabriel@gmail.com>
2024-04-25 20:59:26 +0100

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from './table-basic-example';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
